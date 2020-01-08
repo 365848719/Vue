b176a649de47e1290ae1dc53f590f28c36f94e41
施成志 <365848719@qq.com>
2020-01-08 22:57:38 +0800

    使用vuex

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,20 +1,43 @@
 <template>
   <div class="hello">
+    <h1 style="color:red">vuex：State测试：{{this.$store.state.count }}</h1>
+    <h1 style="color:blue">vuex：Getter测试：{{this.$store.getters.getStateCount }}</h1>
+
+    <h1>vuex：mutations 测试</h1>
+    <button @click="add" style="width:150px;height:50px">+</button>
+    <br />
+    <br />
+    <button @click="reduce" style="width:150px;height:50px">-</button>
+    <br />
+    <br />
+    <h1>vuex：Actions 测试</h1>
+    <button @click="multi(10)" style="width:150px;height:50px">*</button>
+
     <h1>{{ msg }}</h1>
-    <p>
-      {{myMsg}}  
-    </p>
+    <p>{{myMsg}}</p>
     <h2>你好啊！</h2>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Component, Prop, Vue } from "vue-property-decorator";
 
 @Component
 export default class HelloWorld extends Vue {
   @Prop() private msg!: string;
   @Prop() private myMsg!: string;
+
+  add() {
+    this.$store.commit("add");
+  }
+
+  reduce() {
+    this.$store.commit("reduce");
+  }
+
+  multi(n: number) {
+    this.$store.dispatch("multiAsync", n);
+  }
 }
 </script>
 
--- src/store/index.ts
+++ src/store/index.ts
@@ -5,10 +5,27 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    count: 1
+  },
+  getters: {
+    getStateCount: (state: any): number => { return state.count + 1; }
   },
   mutations: {
+    add: (state: any): number => { return state.count++; },
+    reduce: (state: any): number => { return state.count--; },
+    multi: (state: any, n: number): number => {
+      let result = state.count * n;
+      console.log("result = " + result);
+
+      state.count = result;
+      return result;
+    }
   },
   actions: {
+    multiAsync: (context: any, obj: any) => {
+      context.commit("multi", obj);
+    }
+
   },
   modules: {
   }
